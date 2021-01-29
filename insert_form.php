a040a96e2da0f7131c932127081f253f883b0cfc
Sutthichai <new001230013@gmail.com>
2021-01-29 13:46:15 +0700

    insert_from

---
+++ insert_form.php
@@ -0,0 +1,12 @@
+<html>
+	<body>
+	
+	<form action="insert.php" method="post">
+		ชื่อ:<input type="text" name="firstname" /> <br/>
+		นามสกุล:<input type="text" name="lastname" /> <br/>
+		email :<input type="email" id="email" name="email"/> <br/>
+		
+		<input type="submit" value="บันทึก" name="submit_btn" />
+	</form>
+	</body>
+</html>
